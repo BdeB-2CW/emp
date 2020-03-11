9684fb0d2622027e343cbb2e61746e208c6b11e6
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2020-03-11 16:31:44 -0400

    test

--- scripts22.js
+++ scripts22.js
@@ -1,14 +1,3 @@
-/*const url = "http://localhost:8080/ords/hr2/employees";
-let response = await fetch(url);
-
-if (response.ok) {
-  // if HTTP-status is 200-299
-  // get the response body (the method explained below)
-  let json = await response.json();
-  console.log(json);
-} else {
-  alert("HTTP-Error: " + response.status);
-}*/
 function createNode(element) {
   return document.createElement(element);
 }
