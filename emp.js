3c5894a51900c2760f2f9474d11f1f29835a63c2
Jean-François Brodeur <jifbrodeur@jbmlogic.com>
2020-05-13 15:00:08 -0400

    Working with cloud

--- emp.js
+++ emp.js
@@ -7,7 +7,9 @@ function append(parent, el) {
 }
 
 const ul = document.getElementById("employees");
-const url = "http://localhost:8080/ords/hr2/employees";
+//const url = "http://localhost:8080/ords/hr2/employees";
+const url =
+  "https://uv0ncfhhdwwjeyx-db202001241215.adb.ca-toronto-1.oraclecloudapps.com/ords/hr2/employees";
 fetch(url)
   .then((resp) => resp.json())
   .then(function (data) {
@@ -23,3 +25,5 @@ fetch(url)
   .catch(function (error) {
     console.log(JSON.stringify(error));
   });
+
+  
